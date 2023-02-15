2d7d40c62d438d958d9833fabcdaf3ba2664062c
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-02-15 18:53:47 +0900

    Create function for toggling class change upon hovering in each box

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let gridMotherBox = document.querySelector('.grid-mother-box')
+const gridMotherBox = document.querySelector('.grid-mother-box')
 
 for(let i = 1; i <= 256; i++){
     let newDiv = document.createElement('div')
     newDiv.className = "grid-box"
     gridMotherBox.appendChild(newDiv)
 }
+    const boxes = document.querySelectorAll('.grid-box')
+    
+    boxes.forEach((box) => 
+        box.addEventListener('mouseenter', function() {
+        box.classList.toggle('grid-box-color-change')
+    })
+    )
+    
