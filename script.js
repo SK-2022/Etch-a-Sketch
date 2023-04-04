1ed5d5dd30b7301bbe860cf8252d48799db04b78
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-04-04 14:20:48 +0900

    Complete final edit of function. Change box fill in color to blue.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const gridMotherBox = document.querySelector('.grid-mother-box')
-//Creates 256(16 X 16) boxes all with the class 'grid box' and appends each of them to the div with the 'grid-mother-box' class
-for(let i = 1; i <= (24) ** 2; i++){
-    let square = document.createElement('div')
-    square.className = "grid-box"
-    gridMotherBox.appendChild(square)
-}
-    // Select all elements with the class of 'grid-box'
+
+let promptButton = document.querySelector('.change-grid-size')
+
+promptButton.addEventListener('click', () => {
+    let gridNumber = window.prompt("How many squares on each side of the grid?")
+    if(gridNumber < 0 || gridNumber > 100) {
+        return window.alert("Your number needs to be between 0 and 100")
+    }
+
+    //This adds the styling of gridTemplateColumns and gridTemplateRows to the gridMotherBox
+    gridMotherBox.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
+    gridMotherBox.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
+
+    //Creates 256(16 X 16) boxes all with the class 'grid box' and appends each of them to the div with the 'grid-mother-box' class
+    for(let i = 1; i <= (gridNumber) ** 2; i++){
+        let square = document.createElement('div')
+        square.className = "grid-box"
+        gridMotherBox.appendChild(square)
+    }
+ 
+   // Select all elements with the class of 'grid-box'
     const boxes = document.querySelectorAll('.grid-box')
     
     //For each item selected(can be any name in () but I have conveniently named it 'box'), add an event
@@ -15,10 +29,5 @@ for(let i = 1; i <= (24) ** 2; i++){
         box.classList.add('grid-box-color-change')
     }))
 
-    let promptButton = document.querySelector('.change-grid-size')
-    promptButton.addEventListener('click', () => {
-        let gridNumber = window.prompt("How many squares on each side of the grid?")
-        if(gridNumber < 0 || gridNumber > 100){
-            window.alert("Your number needs to be between 0 and 100")
-        }
-    })
+})
+
