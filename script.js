3e481c0c2a2e721298a6158450f622c6b7fd8781
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-04-10 19:29:04 +0900

    Add default box that appears when page is loaded for the first time.

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const gridMotherBox = document.querySelector('.grid-mother-box')
 
+gridMotherBox.style.gridTemplateColumns = `repeat(16, 1fr)`
+gridMotherBox.style.gridTemplateRows = `repeat(16, 1fr)`
+    //DEFAULT BOX UPON PAGE REFRESH
+
+    //Creates new 16 X 16 Grid with 256 boxes all with the class 'grid box' and appends each of them to the div with the 'grid-mother-box' class
+    for(let i = 1; i <= (16) ** 2; i++){
+        let square = document.createElement('div')
+        square.className = "grid-box"
+        gridMotherBox.appendChild(square)
+    }
+ 
+   // Select all elements with the class of 'grid-box'
+    const boxes = document.querySelectorAll('.grid-box')
+    
+    //For each item selected(can be any name in () but I have conveniently named it 'box'), add an event
+    // listener that causes a new class 'grid-box-color-change' to be added.
+    boxes.forEach((box) => 
+        box.addEventListener('mouseenter', function() {
+        box.classList.add('grid-box-color-change')
+    }))
+
+
+
 let promptButton = document.querySelector('.change-grid-size')
 
 promptButton.addEventListener('click', () => {
@@ -7,12 +30,11 @@ promptButton.addEventListener('click', () => {
     if(gridNumber < 0 || gridNumber > 100) {
         return window.alert("Your number needs to be between 0 and 100")
     }
-
     //This adds the styling of gridTemplateColumns and gridTemplateRows to the gridMotherBox
     gridMotherBox.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
     gridMotherBox.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
 
-    //Creates 256(16 X 16) boxes all with the class 'grid box' and appends each of them to the div with the 'grid-mother-box' class
+    //Creates new grid of the size 'gridNumber X gridNumber' boxes all with the class 'grid box' and appends each of them to the div with the 'grid-mother-box' class
     for(let i = 1; i <= (gridNumber) ** 2; i++){
         let square = document.createElement('div')
         square.className = "grid-box"
